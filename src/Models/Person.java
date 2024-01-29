fd221a11cb3a06139d2ddd17ded4d55bcd5d5661
NurasylKh <153474385+NurasylKh@users.noreply.github.com>
2024-01-29 10:09:31 +0600

    Person.java

--- src/Models/Person.java
+++ src/Models/Person.java
@@ -2,43 +2,43 @@ package Models;
 
 import interfaces.Payable;
 
-public abstract class Person implements Payable, Comparable<Person>{
+public abstract class Person implements Payable, Comparable<Person>{ //declaring abstact class
     static int idCounter = 1;
     int id;
     String name, surname;
 
-    public Person(){
+    public Person(){ // creating default constructor
         id = idCounter++;
         name = "Name";
         surname = "Surname";
     }
-    public Person(String name, String surname){
+    public Person(String name, String surname){ // creating constructor that takes 2 arguments
         id = idCounter++;
         this.name = name;
         this.surname = surname;
     }
 
-    public void setName(String name) {
+    public void setName(String name) { //setter method
         this.name = name;
     }
-    public void setSurname(String surname){
+    public void setSurname(String surname){ // setter method
         this.surname = surname;
     }
-    public int getId(){
+    public int getId(){ //getter method
         return id;
     }
-    public String getName(){
+    public String getName(){ //setter method
         return name;
     }
-    public String getSurname(){
+    public String getSurname(){ //getter method
         return surname;
     }
 
-    @Override public String toString(){
+    @Override public String toString(){ //representation of objecta as a String
         return id + ". " + name + " " + surname;
     }
-    public abstract String getPosition();
-    public String printData(){
+    public abstract String getPosition(); //getter method
+    public String printData(){ //method that increments
         return toString() + " earns " + getPaymentAmount() + " tenge.";
     }
 }
