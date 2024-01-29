b37d2625c48a73a18ca77e8f0755c900d6476ad2
NurasylKh <153474385+NurasylKh@users.noreply.github.com>
2024-01-29 10:08:54 +0600

    Employee.java

--- src/Models/Employee.java
+++ src/Models/Employee.java
@@ -1,70 +1,72 @@
 package Models;
 
-public class Employee extends Person{
+public class Employee extends Person{ //declaring class // Employee is a subclass of Person
     String position;
     double salary;
-    public Employee(){
-        super();
+    public Employee(){ //creating constructor
+        super(); // calling superclass s constructor
         position = "Position";
         salary = 0;
     }
-    public Employee(String name, String surname, String position, double salary){
+    public Employee(String name, String surname, String position, double salary){ // creating constructor that takes 3 arguments
         super(name, surname);
         this.position = position;
         this.salary = salary;
     }
 
-    @Override public int getId() {
+    @Override
+    public int getId() { // getter method
         return super.getId();
     }
 
-    @Override public String getName(){
+    @Override
+    public String getName(){ // getter method
         return super.getName();
     }
 
     @Override
-    public String getSurname() {
+    public String getSurname() { // getter method
         return super.getSurname();
     }
 
-    public String getPosition() {
+    public String getPosition() { //getter method
         return position;
     }
 
-    public double getSalary(){
+    public double getSalary(){ //getter method
         return salary;
     }
 
     @Override
-    public void setName(String name) {
+    public void setName(String name) { //setter method
         super.setName(name);
     }
 
     @Override
-    public void setSurname(String surname) {
+    public void setSurname(String surname) { //setter method
         super.setSurname(surname);
     }
 
-    public void setPosition(String position) {
+    public void setPosition(String position) { //setter method
         this.position = position;
     }
 
-    public void setSalary(double salary) {
+    public void setSalary(double salary) { //setter method
         this.salary = salary;
     }
 
     @Override
-    public String toString() {
+    public String toString() { //representaion of object as String
         return "Employee: " + super.toString();
     }
 
     @Override
-    public double getPaymentAmount() {
+    public double getPaymentAmount() { //getter method
         return salary;
     }
 
     @Override
-    public int compareTo(Person person) {
+    public int compareTo(Person person) { // it goes through a collection of persons and compares two values
         if(salary == person.getPaymentAmount()) return 0;
         else if(salary > person.getPaymentAmount()) return 1;
         else return -1;
