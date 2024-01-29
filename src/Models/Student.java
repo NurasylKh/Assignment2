04944ea9d9cdbd80bf47c72d839a9856e96bae89
NurasylKh <153474385+NurasylKh@users.noreply.github.com>
2024-01-29 10:12:30 +0600

    Student.java

--- src/Models/Student.java
+++ src/Models/Student.java
@@ -1,66 +1,66 @@
 package Models;
 
-public class Student extends Person {
+public class Student extends Person { // Student is a subclass of Person
     double gpa;
 
     public Student(){
         super();
         gpa = 0;
     }
-    public Student(String name, String surname, double gpa){
+    public Student(String name, String surname, double gpa){ //constructor that takes 3 arguments
         super(name, surname);
-        this.gpa = gpa;
+        setGpa(gpa);
     }
 
     @Override
-    public String getSurname() {
+    public String getSurname() { //getter method
         return super.getSurname();
     }
 
     @Override
-    public int getId() {
+    public int getId() { //getter method
         return super.getId();
     }
 
     @Override
-    public String getName() {
+    public String getName() { //getter method
         return super.getName();
     }
 
-    public double getGpa() {
+    public double getGpa() { //getter method
         return gpa;
     }
 
     @Override
-    public void setName(String name) {
+    public void setName(String name) { //setter method
         super.setName(name);
     }
 
     @Override
-    public void setSurname(String surname) {
+    public void setSurname(String surname) { //setter method
         super.setSurname(surname);
     }
 
-    public void setGpa(double gpa) {
+    public void setGpa(double gpa) { //setter method
         this.gpa = gpa;
     }
 
     @Override
-    public String toString() {
+    public String toString() { //representation of object as a String
         return "Student: " + super.toString();
     }
 
     @Override
-    public String getPosition() {
+    public String getPosition() { //getter method
         return "Student";
     }
 
     @Override
-    public double getPaymentAmount() {
+    public double getPaymentAmount() { //getter method
         return gpa > 2.67 ? 36660.0 : 0;
     }
 
-    @Override public int compareTo(Person person){
+    @Override public int compareTo(Person person){ //method that goes through a collection of persons and compares two values
         if(getPaymentAmount() == person.getPaymentAmount()) return 0;
         else if (getPaymentAmount() > person.getPaymentAmount()) return 1;
         else return -1;
